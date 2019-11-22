9d5dfa107a1f0259146460530c64a79b781778e9
Anton Hlinisty <anton.hlinisty@gmail.com>
2019-11-22 23:43:11 +0300

    Integrate with config service

--- src/integration-test/java/club/tempvs/profile/controller/ProfileControllerIntegrationTest.java
+++ src/integration-test/java/club/tempvs/profile/controller/ProfileControllerIntegrationTest.java
@@ -10,6 +10,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
@@ -29,6 +30,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @Transactional
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 @RunWith(SpringRunner.class)
 public class ProfileControllerIntegrationTest {
 
